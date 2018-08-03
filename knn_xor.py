d576c1bf08a5f26709e623d7d6b285b6de3ed600
ElimuMichael <elimumichael9@gmail.com>
2018-08-03 14:38:07 +0800

    The Utility File

---
+++ knn_xor.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+from util import get_xor
+from knn import KNN
+
+if __name__=='__main__':
+   X, Y  = get_xor()
+   
+   plt.scatter(X[:,0], X[:, 1], s = 100, c = Y, alpha = 0.5)
+   plt.show()
+   
+   for k in (1, 2, 3, 4, 5):
+       model = KNN(k)
+       model.fit(X, Y)
+       print("Model with {} neighbor: - Accuracy: {}".format(k, model.score(X, Y)))
